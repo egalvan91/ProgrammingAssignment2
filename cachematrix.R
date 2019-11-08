6f3343113de4293f10115eb0c0c151f3f60974c4
elgalvan <egalvan@ergorenova.com>
2019-11-07 22:37:51 -0300

    Version Final

--- cachematrix.R
+++ cachematrix.R
@@ -1,45 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are my functions that cache the inverse of an array. By elgalvan - 2019/11/07
+
+## Loading the mathlib package, this will allow you to use the "inv ()" function to calculate the inverse matrix
 
 library(matlib)
 
-## Write a short comment describing this function
+## Create a special "matrix", which is really a list that contains a function to:
+## - set the value of the matrix
+## - get the value of the matrix
+## - set the value of the inverse matrix
+## - get the value of the inverse matrix
+
+## Similar to the example
 
 makeCacheMatrix <- function(x = matrix()) {
 
-    m <- NULL
+    i <- NULL
 
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        i <<- NULL
     }
 
     get <- function() x
 
-    setmean <- function(mean) m <<- mean
+    setinv <- function(inv) i <<- inv
 
-    getmean <- function() m
+    getinv <- function() i
 
-    list(set = set, get = get, setmean = setmean, getmean = getmean)
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
-## Write a short comment describing this function
+## The following function calculates the inverse matrix of the special "matrix" 
+## Created with the previous function. However, first check if it has already been 
+## Calculated. If so, get the reverse array from the cache and skip the calculation. 
+## Otherwise, it calculates the inverse matrix of the data and sets the value of 
+## The inverse matrix in the cache using the setinv function.
+
+## Similar to the example but instead of using the mean () function, we use the inv () function
 
 cacheSolve <- function(x, ...) {
-    m <- x$getmean()
+    i <- x$getinv()
 
-    if(!is.null(m)) {
+    if(!is.null(i)) {
             message("getting cached data")
-            return(m)
+            return(i)
     }
 
     data <- x$get()
 
-    m <- inv(data, ...)
+    i <- inv(data, ...)
 
-    x$setmean(m)
+    x$setinv(i)
 
-    m
+    i
 
 }
